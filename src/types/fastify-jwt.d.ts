fe4e0edcc9decf2cef805b943bd3563abc126fb2
tdugini <105417196+tdugini@users.noreply.github.com>
2026-08-25 10:54:34 +0200

    refactor: type JWT claims explicitly

--- src/types/fastify-jwt.d.ts
+++ src/types/fastify-jwt.d.ts
@@ -4,13 +4,17 @@ declare module "@fastify/jwt" {
   interface FastifyJWT {
     payload: {
       id: number;
-      role: string;
+      role: "user" | "admin";
+      tokenType: "access" | "refresh";
+      issuedAt: number;
       jti?: string;
     };
     user: {
       id: number;
-      role: string;
-      iat: number;
+      role: "user" | "admin";
+      tokenType: "access" | "refresh";
+      issuedAt: number;
+      jti?: string;
     };
   }
 }
